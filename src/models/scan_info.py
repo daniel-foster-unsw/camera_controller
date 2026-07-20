d7a9c9d1b6401b0998847d8866906aba743ffb39
Daniel Foster <daniel.foster@unsw.edu.au>
2026-07-20 17:53:08 +1000

    Sprint 12.8.2 memory management scan commands unit test error fixes

--- src/models/scan_info.py
+++ src/models/scan_info.py
@@ -3,15 +3,17 @@ from dataclasses import dataclass
 
 @dataclass
 class ScanInfo:
-    name: str
+    
     """
     Information about a scan returned by the protocol.
     """
-
+    name: str
 
     
-
+    """
     def __init__(self, scan):
 
         self.name = scan.folder_name
         #self.image_count = scan.image_count
+
+    """
